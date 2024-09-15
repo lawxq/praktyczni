e16ce3022bd20455f5f9beb02036a4af9326ba01
lawxq <MUKHAMADIEWA20@GMAIL.COM>
2024-09-16 02:23:25 +0300

    Add files via upload

---
+++ ПРАКТИЧНА1-2.py
@@ -0,0 +1,12 @@
+name = "Romana"
+secondname = "Mukhamadiieva"
+a = "hello world"
+age = 16
+print(name)
+print(secondname)
+print(age)
+print(a)
+print(type(name))
+print(type(secondname))
+print(type(a))
+print(type(age))
