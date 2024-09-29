7e910c2a96f7f3e38040034b478758bf0c3de906
lawxq <MUKHAMADIEWA20@GMAIL.COM>
2024-09-29 23:43:23 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,18 @@
+name = "Romana"
+secondname = "Mukhamadiieva"
+a = "hello world"
+age = 16
+list1 = [name]
+if type(name)==type(secondname):
+    print(type(name))
+    list1 = ["Romana Mukhamadiieva"]
+    if type(age) == int:
+        print(int)
+
+
+
+
+
+
+
+dict = {1} (10,14), {2}(29 , 2), {3}(3,36), {4}(45,4), {5}(5,50), {6}(63,6),
---
+++ 3.py
@@ -0,0 +1,5 @@
+number = {1:"10", 2:"28", 3:"ad", 4:"ww", 5:"80", 6:"q", 7:"i", 8:"65" , 9:"77" , 10:"5"}
+number.update({2:38})
+number.update({7:4})
+number.pop(3)
+number.update({4:0})
