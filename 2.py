e8fb2622da6a74b5280e76af4439e4d45ded81b3
lawxq <MUKHAMADIEWA20@GMAIL.COM>
2024-10-26 12:57:24 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,6 +5,6 @@ age = 16
 list1 = [name]
 if type(name)==type(secondname):
     print(type(name))
-    list1 = ["Romana Mukhamadiieva"]
+    list1 = name + secondname
     if type(age) == int:
         print(int)
